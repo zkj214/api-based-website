a5384b61b848bbb5261902f582fb0ef25d063312
Ziebhar <pythonexercise214@gmail.com>
2024-04-06 08:34:57 +0800

    update home function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from flask import Flask,render_template,request
 from flask_bootstrap import Bootstrap5 #pip install bootstrap-flask
 from flask_wtf import FlaskForm
@@ -6,7 +7,6 @@ from wtforms.validators import DataRequired
 import requests
 import os
 from datetime import date
-import time
 
 
 app=Flask(__name__)
@@ -26,32 +26,30 @@ def home():
     #form=AskChatbotForm()
     if request.method=="POST":
         user_query=request.form.get("query")
+        url = "https://robomatic-ai.p.rapidapi.com/api"
+
+        payload = {
+            "in": f"{user_query}",
+            "op": "in",
+            "cbot": "1",
+            "SessionID": "RapidAPI1",
+            "cbid": "1",
+            "key": os.environ.get("BOT_KEY"),
+            "ChatSource": "RapidAPI",
+            "duration": "1"
+        }
+        headers = {
+            "content-type": "application/x-www-form-urlencoded",
+            "X-RapidAPI-Key": os.environ.get("API_KEY"),
+            "X-RapidAPI-Host": "robomatic-ai.p.rapidapi.com"
+        }
+
+        response = requests.post(url, data=payload, headers=headers)
+
+        data=response.json()["out"]
         return render_template("index.html", user=True, year=current_year, user_txt=user_query)
-        
-        if user_query:
-            time.sleep(3)
-            url = "https://robomatic-ai.p.rapidapi.com/api"
-    
-            payload = {
-                "in": f"{user_query}",
-                "op": "in",
-                "cbot": "1",
-                "SessionID": "RapidAPI1",
-                "cbid": "1",
-                "key": os.environ.get("BOT_KEY"),
-                "ChatSource": "RapidAPI",
-                "duration": "1"
-            }
-            headers = {
-                "content-type": "application/x-www-form-urlencoded",
-                "X-RapidAPI-Key": os.environ.get("API_KEY"),
-                "X-RapidAPI-Host": "robomatic-ai.p.rapidapi.com"
-            }
-    
-            response = requests.post(url, data=payload, headers=headers)
-    
-            data=response.json()["out"]
-            return render_template("index.html",response=data,user=True,bot=True,year=current_year,user_txt=user_query)
+        time.sleep(3)
+        return render_template("index.html",response=data,user=True,bot=True,year=current_year,user_txt=user_query)
     return render_template("index.html",year=current_year)
 
 
